25836a50317687b34eda716ec1eb2521eb42f1c9
Ryan P <103454477+RyanAPierce451@users.noreply.github.com>
2024-05-01 14:57:40 -0500

    Create booleans.java

---
+++ Booleans/booleans.java
@@ -0,0 +1,19 @@
+public class booleans
+{
+	public static void main (String[] args)
+	{
+		boolean summer = true;
+		boolean wearingshorts = false;
+
+		if (summer == true && wearingshorts == false)
+		{
+			System.out.println ("It's going to be hot");
+		}
+		else
+		{
+			System.out.println ("It's fine");
+		}
+	}
+
+
+}
