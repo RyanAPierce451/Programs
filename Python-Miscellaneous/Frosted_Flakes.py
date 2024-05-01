9386e81778cf581baa240324ccaa150ac1d14069
Ryan P <103454477+RyanAPierce451@users.noreply.github.com>
2024-05-01 14:48:41 -0500

    Add files via upload

---
+++ Python-Miscellaneous/Frosted_Flakes.py
@@ -0,0 +1,9 @@
+print('Which Cereal Frosted Flakes or Honey Bunches')
+Cereal = input('Enter F for Frosted or H for Honey ')
+
+if Cereal == 'F':
+    print('Frosted Flakes for the win')
+
+if Cereal == 'H':
+    print('Honey Bunches Win')
+
